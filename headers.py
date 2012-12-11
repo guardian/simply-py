89b0cdcac7190b3b47b0e4f1a1fbec8e75ddbac6
Robert Rees <robert.rees@guardian.co.uk>
2012-12-11 16:44:07 +0000

    Adds the JSON Content headers

--- headers.py
+++ headers.py
@@ -7,3 +7,6 @@ def set_cors_headers(response):
 def set_cache_headers(response, cache_time):
     response.headers['Cache-Control'] = 'public, max-age=' + str(cache_time) + ', stale-if-error=604800, stale-while-revalidate=60'
     response.headers['Date'] = str(time.time())
+
+def json(response):
+	response.headers['Content-Type'] = 'application/javascript'
