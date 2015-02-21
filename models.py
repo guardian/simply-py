a66a6c9b149a638a750bdd5cffba01d8b2985e0b
Robert Rees <robert.rees@guardian.co.uk>
2015-02-21 22:54:19 +0000

    Makes both values of the configuration mandatory as the content does not make sense otherwise

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import ndb
 
 class Configuration(ndb.Model):
-	key = ndb.StringProperty()
-	value = ndb.StringProperty()
+	key = ndb.StringProperty(required=True)
+	value = ndb.StringProperty(required=True)
